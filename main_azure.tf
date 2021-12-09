bc7daf36ed201d671e8027ae2350ca3c8100860f
peterk13 <94086841+peterk13@users.noreply.github.com>
2021-12-09 10:32:40 +1000

    Update main_azure.tf

--- main_azure.tf
+++ main_azure.tf
@@ -22,3 +22,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "Central US"
   resource_group_name = "<ADD YOUR RESOURCE GROUP NAME>"
 }
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                  = "Robins"
+  resource_group_name   = "<ADD YOUR RESOURCE GROUP NAME>"
+  virtual_network_name  = azurerm_virtual_network.vnet.name
+  address_prefix        = "10.0.1.0/24"
+}
